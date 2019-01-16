dea68b5f795f74118ca38aa27dabfc9a72c97aa7
bzk2016 <1427249899@qq.com>
2019-01-16 23:13:43 +0800

    多页面测试

---
+++ src/pages/next/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    {{bzk}}
+    <test/>
+  </div>
+</template>
+
+<script>
+  import Test from '../test/test.js'
+  export default {
+    name: 'App',
+    components: {
+      Test
+    },
+    data(){
+      return {
+          bzk: '凌影'
+      }
+    }
+  }
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/pages/test/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    {{bzk}}
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'App',
+    data(){
+      return {
+          bzk: '白志锟'
+      }
+    }
+  }
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
